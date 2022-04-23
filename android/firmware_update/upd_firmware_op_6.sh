b950b513d0be030ecead55c5f95b395fd30bbb11
Grell Gragham <vrt.epm@protonmail.com>
2022-04-23 18:03:46 +0300

    Add firmware update for OP 6

---
+++ android/firmware_update/upd_firmware_op_6.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+fastboot flash aop_a aop.img
+fastboot flash aop_b aop.img
+fastboot flash bluetooth_a bluetooth.img
+fastboot flash bluetooth_b bluetooth.img
+fastboot flash boot_a boot.img
+fastboot flash boot_b boot.img
+fastboot flash dsp_a dsp.img
+fastboot flash dsp_b dsp.img
+fastboot flash dtbo_a dtbo.img
+fastboot flash dtbo_b dtbo.img
+fastboot flash fw_4j1ed_a fw_4j1ed.img
+fastboot flash fw_4j1ed_b fw_4j1ed.img
+fastboot flash fw_4u1ea_a fw_4u1ea.img
+fastboot flash fw_4u1ea_b fw_4u1ea.img
+fastboot flash modem_a modem.img
+fastboot flash modem_b modem.img
+fastboot flash oem_stanvbk oem_stanvbk.img
+fastboot flash qupfw_a qupfw.img
+fastboot flash qupfw_b qupfw.img
+fastboot flash storsec_a storsec.img
+fastboot flash storsec_b storsec.img
+fastboot flash system_a system.img
+fastboot flash system_b system.img
+fastboot flash vbmeta_a vbmeta.img
+fastboot flash vbmeta_b vbmeta.img
+fastboot flash vendor_a vendor.img
+fastboot flash vendor_b vendor.img
+fastboot flash LOGO_a LOGO.img
+fastboot flash LOGO_b LOGO.img
+
