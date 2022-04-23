d1d7ca42667edb0c496a7695ed9eac0795f1c437
Grell Gragham <vrt.epm@protonmail.com>
2022-04-23 17:26:52 +0300

    Add app data cleaner

---
+++ android/clear_app_data/clear_app_data.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+source "../get_user_uid.sh"
+
+SELECTED_UID=$?
+FILE="$(cat "../pkgs_$SELECTED_UID.txt")"
+SELECTED_PKG=""
+
+killApp() {
+    adb shell am force-stop --user all "$1"
+}
+
+clearData() {
+    adb shell pm clear --user "$SELECTED_UID" "$1"
+}
+
+IFS=$'\n'
+for SELECTED_PKG in $FILE; do
+    # If clear this net.typeblog.shelter, workprofile will be broken
+    if [ "$SELECTED_PKG" = "net.typeblog.shelter" ]; then
+        continue
+    fi
+    killApp "$SELECTED_PKG"
+    echo -e "$SELECTED_PKG killed"
+    clearData "$SELECTED_PKG"
+done
