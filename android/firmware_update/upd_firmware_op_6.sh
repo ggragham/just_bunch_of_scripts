1dc092d254bb515e8a566128bca1f9ae4a3dfa7f
Grell Gragham <vrt.epm@protonmail.com>
2023-01-02 19:23:56 +0200

    feat: op6 update fw script

--- android/firmware_update/upd_firmware_op_6.sh
+++ android/firmware_update/upd_firmware_op_6.sh
@@ -1,32 +1,21 @@
 #!/usr/bin/env bash
 
-fastboot flash aop_a aop.img
-fastboot flash aop_b aop.img
-fastboot flash bluetooth_a bluetooth.img
-fastboot flash bluetooth_b bluetooth.img
-fastboot flash boot_a boot.img
-fastboot flash boot_b boot.img
-fastboot flash dsp_a dsp.img
-fastboot flash dsp_b dsp.img
-fastboot flash dtbo_a dtbo.img
-fastboot flash dtbo_b dtbo.img
-fastboot flash fw_4j1ed_a fw_4j1ed.img
-fastboot flash fw_4j1ed_b fw_4j1ed.img
-fastboot flash fw_4u1ea_a fw_4u1ea.img
-fastboot flash fw_4u1ea_b fw_4u1ea.img
-fastboot flash modem_a modem.img
-fastboot flash modem_b modem.img
-fastboot flash oem_stanvbk oem_stanvbk.img
-fastboot flash qupfw_a qupfw.img
-fastboot flash qupfw_b qupfw.img
-fastboot flash storsec_a storsec.img
-fastboot flash storsec_b storsec.img
-fastboot flash system_a system.img
-fastboot flash system_b system.img
-fastboot flash vbmeta_a vbmeta.img
-fastboot flash vbmeta_b vbmeta.img
-fastboot flash vendor_a vendor.img
-fastboot flash vendor_b vendor.img
-fastboot flash LOGO_a LOGO.img
-fastboot flash LOGO_b LOGO.img
-
+fastboot flash --slot=all abl abl.img
+fastboot flash --slot=all aop aop.img
+fastboot flash --slot=all bluetooth bluetooth.img
+fastboot flash --slot=all cmnlib cmnlib.img
+fastboot flash --slot=all cmnlib64 cmnlib64.img
+fastboot flash --slot=all devcfg devcfg.img
+fastboot flash --slot=all dsp dsp.img
+fastboot flash --slot=all fw_4j1ed fw_4j1ed.img
+fastboot flash --slot=all fw_4u1ea fw_4u1ea.img
+fastboot flash --slot=all hyp hyp.img
+fastboot flash --slot=all keymaster keymaster.img
+fastboot flash --slot=all LOGO LOGO.img
+fastboot flash --slot=all modem modem.img
+fastboot flash --slot=all oem_stanvbk oem_stanvbk.img
+fastboot flash --slot=all qupfw qupfw.img
+fastboot flash --slot=all storsec storsec.img
+fastboot flash --slot=all tz tz.img
+fastboot flash --slot=all xbl xbl.img
+fastboot flash --slot=all xbl_config xbl_config.img
